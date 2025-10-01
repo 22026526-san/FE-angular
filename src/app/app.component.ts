3695b234f7f04b0cc9b0b11b50d7ce8e25e988aa
22026526-san <sangv6548@gmail.com>
2025-10-01 17:15:15 +0700

    feat: add custom Angular Material theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MATERIAL_MODULES } from './material/material-module';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MATERIAL_MODULES],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
